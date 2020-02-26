8311885a2bc5ca058d629b4316c545ae5fd78a78
Matthew Taylor <majikpig@gmail.com>
2020-02-26 18:31:38 -0500

    possession radios

--- src/App.js
+++ src/App.js
@@ -31,16 +31,21 @@ function useInterval(callback, delay) {
 
 function App() {
   //TODO: STEP 2 - Establish your application's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const homeTeam = 'Lions';
+  const awayTeam = 'Tigers';
   const [scoreHome, setScoreHome] = useState(0);
   const [scoreAway, setScoreAway] = useState(0);
   const [timerMin, setTimerMin] = useState(15);
   const [timerSec, setTimerSec] = useState(0);
   const [timerIsRunning, setTimerIsRunning] = useState(false);
   const [readyForExtraPoint, setReadyForExtraPoint] = useState(false);
-
+  //const [ballPosition, setBallPosition] = useState();
+  //const [yardsForFirstDown, setYardsForFirstDown] = useState(10);
+  //const [narrative, setNarrative] = useState();
+  const [teamWithPossession, setTeamWithPossession] = useState(homeTeam);
 
   const tick = () => {
-    console.log(`Tick: ${timerMin}:${timerSec} ${timerInterval}`);
+    console.log(`Tick: ${timerMin}:${timerSec}`);
     let m = timerMin;
     let s = timerSec;
     if ((m < 0) || ((m <= 0) && (s <= 0))) {
@@ -57,12 +62,25 @@ function App() {
     }
   };
 
-  let timerInterval = useInterval(tick,timerIsRunning ? 1000 : null);
+  const changePossession = () => {
+      if (teamWithPossession===homeTeam) {
+        setTeamWithPossession(awayTeam);
+      } else {
+        setTeamWithPossession(homeTeam);
+      }
+  };
 
-
+  useInterval(tick,timerIsRunning ? 1000 : null);
 
   const resetTimer = () => { stopTimer(); setTimerMin(15); setTimerSec(0)};
-  const startTimer = () => { setTimerIsRunning(true) };
+
+  const startTimer = () => {
+      if (timerMin <= 0 && timerSec <= 0) {
+        resetTimer();
+      }
+      setTimerIsRunning(true)
+    };
+
   const stopTimer = () => { setTimerIsRunning(false) };
 
   const addScore = (score, homeTeam) => {
@@ -77,6 +95,10 @@ function App() {
     } else {
       setScoreAway(scoreAway + score);
     }
+  };
+
+  const submitDetails = (formSubmitEvent) => {
+      formSubmitEvent.preventDefault();
   };
 
   let extraPointButton1 = '';
@@ -97,21 +119,29 @@ function App() {
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">{ homeTeam }</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">{ scoreHome }</div>
+            <div className="home__score">{ scoreHome.pad() }</div>
           </div>
           <div className="timer">{ timerMin.pad() }:{ timerSec.pad() }</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{ scoreAway }</div>
+            <h2 className="away__name">{ awayTeam }</h2>
+            <div className="away__score">{ scoreAway.pad() }</div>
           </div>
         </div>
         <BottomRow />
       </section>
       <section className="buttons">
+        <form onSubmit={(formSubmitEvent)=>submitDetails(formSubmitEvent)}>
+        {/*  <label>Ball position: </label><input defaultValue={ballPosition}></input>
+          <label>Yards for 1st Down: </label><input defaultValue={yardsForFirstDown}></input>*/}
+          <label>Team with Possession: </label><br />
+            <input type="radio" id="home" checked={(teamWithPossession===homeTeam)} onChange={()=>changePossession()}></input><label htmlFor="home"> { homeTeam } </label><br />
+            <input type="radio" id="away" checked={(teamWithPossession===awayTeam)} onChange={()=>changePossession()}></input><label htmlFor="away"> { awayTeam } </label>
+            {/*<input type="button" value="Submit"></input>*/}
+        </form>
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" onClick={()=>addScore(6, true)}>Home Touchdown</button>
